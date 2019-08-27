d89aa87186f15d4d6d83764c2251eec03a26b66e
hiBrush <lordlosslyy@gmail.com>
2019-08-27 15:52:12 +0800

    add function showcam

--- camera.py
+++ camera.py
@@ -6,13 +6,16 @@ class Cam:
         self.cap = cv2.VideoCapture(0)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-    def opencam(self):
+    def opencam(self,save):
         ret, frame = self.cap.read()
+        if save: 
+           cv2.imwrite('./Audience.jpg', frame)
         self.cap.release()
         cv2.destroyAllWindows()
         return frame
+    def showcam(self):
+        aud = cv2.imread('./Audience.jpg')
+        cv2.imshow('My audience', aud)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         
-  # 設定影像的尺寸大小
-  
-
-  
